13c3c1642edeb06a853811e3fd8dee47f4083fdb
Nick Beijerbacht <beijerbachtn@gmail.com>
2022-02-07 17:14:41 +0100

    Toevoeging basis componenten met variables

---
+++ src/Components/Board.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './List'
+
+class Board extends Component {
+    render(){
+        return(
+            <div>
+                Lijst van lijsten
+                <List/>
+            </div>
+        )
+    }
+}
---
+++ src/Components/List.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './Card'
+
+class List extends Component {
+    render(){
+        return(
+            <div>
+                Lijst van kaarten
+                <Card/>
+            </div>
+        )
+    }
+}
